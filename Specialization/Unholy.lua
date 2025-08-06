cfff9bacf18e5bb6912b132f40b7852db5e5e29f
tbkizle <tbkizle@gmail.com>
2025-08-06 16:08:57 -0400

    Update Retail Unholy

--- Specialization/Unholy.lua
+++ Specialization/Unholy.lua
@@ -308,7 +308,7 @@ function Unholy:precombat()
     else
         trinket_2_sync = 0.5
     end
-    if not trinket_1_buffs and trinket_2_buffs and (MaxDps:HasOnUseEffect('14') or not MaxDps:HasOnUseEffect('13')) or trinket_2_buffs and ((MaxDps:CheckTrinketCooldownDuration('14')%trinket_2_duration)*(1.5 + MaxDps:HasBuffEffect('14', 'strength'))*(trinket_2_sync)*(trinket_2_high_value)*(1+((MaxDps:CheckTrinketItemLevel('14') - MaxDps:CheckTrinketItemLevel('13'))%100)))>((MaxDps:CheckTrinketCooldownDuration('13')%trinket_1_duration)*(1.5 + MaxDps:HasBuffEffect('13', 'strength'))*(trinket_1_sync)*(trinket_1_high_value)*(1+((MaxDps:CheckTrinketItemLevel('13') - MaxDps:CheckTrinketItemLevel('14'))%100))) then
+    if not trinket_1_buffs and trinket_2_buffs and (MaxDps:HasOnUseEffect('14') or not MaxDps:HasOnUseEffect('13')) or trinket_2_buffs and ((MaxDps:CheckTrinketCooldownDuration('14')%trinket_2_duration)*(1.5 + (MaxDps:HasBuffEffect('14', 'strength') and 1 or 0))*(trinket_2_sync)*(trinket_2_high_value)*(1+((MaxDps:CheckTrinketItemLevel('14') - MaxDps:CheckTrinketItemLevel('13'))%100)))>((MaxDps:CheckTrinketCooldownDuration('13')%trinket_1_duration)*(1.5 + (MaxDps:HasBuffEffect('13', 'strength') and 1 or 0))*(trinket_1_sync)*(trinket_1_high_value)*(1+((MaxDps:CheckTrinketItemLevel('13') - MaxDps:CheckTrinketItemLevel('14'))%100))) then
         trinket_priority = 2
     else
         trinket_priority = true
